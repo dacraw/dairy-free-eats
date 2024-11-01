afed7c67791fad723ef7bd1972a6fdea4ebd1ade
Doug <doug.a.crawford@gmail.com>
2024-11-01 12:46:59 -0700

    Create order component and frontend route

---
+++ app/javascript/components/Order.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Order = () => {
+  return <p>Welcome to the order page!</p>;
+};
+
+export default Order;
--- app/javascript/routes/index.tsx
+++ app/javascript/routes/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../components/Home";
+import Order from "../components/Order";
 
 const AppRoute = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/order" element={<Order />} />
       </Routes>
     </Router>
   );
