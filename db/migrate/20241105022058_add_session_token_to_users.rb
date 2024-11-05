046a88bf3679949b83ed684e0bfc4705ec3c579e
Doug <doug.a.crawford@gmail.com>
2024-11-04 21:22:01 -0800

    Rubocop

--- db/migrate/20241105022058_add_session_token_to_users.rb
+++ db/migrate/20241105022058_add_session_token_to_users.rb
@@ -3,5 +3,4 @@ class AddSessionTokenToUsers < ActiveRecord::Migration[7.2]
     add_column :users, :session_token, :string, null: false
     add_index :users, :session_token, unique: true
   end
-
 end
