d7647a68fc6d734dc0a282fb524bb89e40ef9746
Doug <doug.a.crawford@gmail.com>
2024-11-04 18:40:14 -0800

    Add session token to users and constraints for recovery password digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_04_181920) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_05_022559) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,7 +20,10 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_04_181920) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "recovery_password_digest"
+    t.string "recovery_password_digest", null: false
+    t.string "session_token", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["recovery_password_digest"], name: "index_users_on_recovery_password_digest", unique: true
+    t.index ["session_token"], name: "index_users_on_session_token", unique: true
   end
 end
