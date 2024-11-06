3f7e6843f080395ad0755a34215c86f2dd541326
Doug <doug.a.crawford@gmail.com>
2024-11-05 21:52:30 -0800

    Create responsive nav

--- app/javascript/components/HeaderNav.tsx
+++ app/javascript/components/HeaderNav.tsx
@@ -1,4 +1,7 @@
-import React, { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useEffect, useRef, useState } from "react";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import {} from "@fortawesome/free-regular-svg-icons";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { getCsrfToken } from "util/formUtil";
 
@@ -13,7 +16,7 @@ type NavProps = {
 
 const DesktopNav: React.FC<NavProps> = ({ currentUser, logout }) => {
   return (
-    <div className="hidden justify-between md:flex">
+    <nav className="hidden justify-between md:flex">
       <div className="gap-4 flex">
         <Link
           className="hover:bg-blue-400 hover:text-gray-100 py-2 px-4 transition-colors rounded font-bold text-gray-700"
@@ -53,15 +56,82 @@ const DesktopNav: React.FC<NavProps> = ({ currentUser, logout }) => {
           </button>
         )}
       </div>
-    </div>
+    </nav>
   );
 };
 
 const ResponsiveNav: React.FC<NavProps> = ({ currentUser, logout }) => {
+  const [showMenu, toggleShowMenu] = useState(false);
+  const location = useLocation();
+  const menuRef = useRef<HTMLDivElement | null>(null);
+  const hamburgerRef = useRef<SVGSVGElement | null>(null);
+
+  useEffect(() => {
+    toggleShowMenu(false);
+  }, [location.pathname]);
+
+  useEffect(() => {
+    const checkCloseMenu = (e: MouseEvent) => {
+      // allow hamburger icon to toggle menu status if it is clicked
+      if (
+        hamburgerRef.current &&
+        hamburgerRef.current.contains(e.target as Node)
+      ) {
+        return;
+      }
+
+      if (menuRef.current && !menuRef.current.contains(e.target as Node)) {
+        toggleShowMenu(false);
+      }
+    };
+    document.addEventListener("mousedown", checkCloseMenu);
+
+    return () => document.removeEventListener("mousedown", checkCloseMenu);
+  }, []);
+
   return (
-    <div className="md:hidden">
-      <Link to="/order">Order Now</Link>
-    </div>
+    <nav>
+      <div className="flex justify-between items-center md:hidden">
+        <Link to="/order">Order Now</Link>
+
+        <div className="text-right">
+          <FontAwesomeIcon
+            ref={hamburgerRef}
+            icon={faBars}
+            size="2xl"
+            onClick={() => {
+              toggleShowMenu(!showMenu);
+            }}
+          />
+          <div className="relative w-28 z-10" ref={menuRef}>
+            <div
+              className={`absolute overflow-hidden bg-white shadow w-full text-center transition-all duration-500 ease-in-out ${
+                showMenu ? "max-h-60" : "max-h-0"
+              }`}
+            >
+              <Link className="block py-1" to="/">
+                Home
+              </Link>
+              <Link className="block py-1" to="/order">
+                Order
+              </Link>
+              {currentUser ? (
+                <button onClick={logout}>Logout</button>
+              ) : (
+                <>
+                  <Link className="block py-1" to="/login">
+                    Login
+                  </Link>
+                  <Link className="block py-1" to="/signup">
+                    Signup
+                  </Link>
+                </>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
   );
 };
 
@@ -107,7 +177,7 @@ const HeaderNav = () => {
   };
 
   return (
-    <header className="border-b-2 my-2 p-2 h-14">
+    <header className="border-b-2 mb-4 p-2">
       <DesktopNav currentUser={currentUser} logout={logout} />
       <ResponsiveNav currentUser={currentUser} logout={logout} />
     </header>
