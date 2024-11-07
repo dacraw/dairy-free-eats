de972e5c9fdd3abb12f51a7d06aff8b900f797ea
Doug <doug.a.crawford@gmail.com>
2024-11-06 20:06:22 -0800

    dry run codegen

--- codegen.ts
+++ codegen.ts
@@ -1,16 +1,15 @@
-
-import type { CodegenConfig } from '@graphql-codegen/cli';
+import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
   overwrite: true,
   schema: "app/javascript/graphql/schema.graphql",
   documents: "app/javascript/**/*.tsx",
   generates: {
-    "app/javascript/graphql/types.tsx": {
+    "app/javascript/graphql/": {
       preset: "client",
-      plugins: []
-    }
-  }
+      plugins: [],
+    },
+  },
 };
 
 export default config;
