06a385450f05da34f48b485ef69d8fc9f3091e63
Doug <doug.a.crawford@gmail.com>
2024-11-09 14:43:06 -0800

    Write Login page Capy spec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     trait :valid_user do
-      email { Faker::Internet.email }
+      email { Faker::Internet.unique.email }
 
       before :create do |user|
         password = Faker::Internet.password min_length: 8
---
+++ spec/features/login_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+
+RSpec.feature "Logins", type: :feature do
+  let!(:user) { create :user, :valid_user }
+
+  context "with valid credentials" do
+    it "redirects to the root page" do
+      visit login_path
+
+      expect(page).to have_content "Email"
+      expect(page).to have_content "Password"
+
+      fill_in "Email", with: user.email
+      fill_in "Password", with: user.password
+      find("input[type='submit']").click
+
+      expect(page).to have_content "Order lactose-free food that is tasty and affordable."
+      expect(page).to have_content "Logged in as: #{user.email}"
+    end
+  end
+
+  context "with invalid credentials" do
+    context "rendering error message" do
+      it "returns error for an invalid email" do
+        visit login_path
+
+        expect(page).to have_content "Email"
+        expect(page).to have_content "Password"
+
+        fill_in "Email", with: Faker::Internet.unique.email
+        fill_in "Password", with: user.password
+        find("input[type='submit']").click
+
+        expect(page).to have_content "Credentials invalid"
+      end
+
+      it "renders error for an invalid password" do
+        visit login_path
+
+        expect(page).to have_content "Email"
+        expect(page).to have_content "Password"
+
+        fill_in "Email", with: user.email
+        fill_in "Password", with: Faker::Internet.password(min_length: 9)
+        find("input[type='submit']").click
+
+        expect(page).to have_content "Credentials invalid"
+      end
+    end
+  end
+end
