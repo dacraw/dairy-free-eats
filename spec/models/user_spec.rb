9cfb217778c516258116d7f43cf947d1c971141a
Doug <doug.a.crawford@gmail.com>
2024-11-11 13:47:05 -0800

    Validate the format of stripe_customer_id on the User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -45,5 +45,46 @@ RSpec.describe User, type: :model do
       expect(user.save).to eq false
       expect(user.errors.full_messages).to include "Email can't be blank"
     end
+
+    context "#stripe_customer_id" do
+      it "allows a value starting with 'cus_'" do
+        stripe_customer_id = "cus_12345"
+        user = build(
+          :user, 
+          password: "password", 
+          password_confirmation: "password", 
+          stripe_customer_id: stripe_customer_id,
+          email: "tester@test.com"
+        )
+
+        expect { user.save }.to change { User.count }.from(0).to(1)
+        expect(user.stripe_customer_id).to eq stripe_customer_id
+      end
+
+      it "disallows a value not starting with 'cus_" do
+        user = build(
+          :user, 
+          password: "password", 
+          password_confirmation: "password", 
+          stripe_customer_id: "this_is_not_right",
+          email: "tester@test.com"
+        )
+
+        expect { user.save }.not_to change { User.count }
+        expect(user.errors.full_messages).to include "Stripe customer does not represent a properly formatted Stripe Customer id."
+      end
+
+      it "does not add an error if the field is blank" do
+        user = build(
+          :user, 
+          password: "password", 
+          password_confirmation: "password", 
+          email: "tester@test.com"
+        )
+
+        expect { user.save }.to change { User.count }.from(0).to(1)
+        expect(user.errors.full_messages).to be_empty
+      end
+    end
   end
 end
