bdff8df0b1cf47a3a55fb2c04fcee33ce1db05d8
Doug <doug.a.crawford@gmail.com>
2024-11-12 13:15:38 -0800

    Change column stripe_id to stripe_payment_intent_id in orders table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_11_12_210452) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_12_210647) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_12_210452) do
     t.string "stripe_payment_intent_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.json "stripe_checkout_session_line_items"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
