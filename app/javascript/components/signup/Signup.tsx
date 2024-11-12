4aae7ae50df9e35f34316527eda578fc2662653c
Doug <doug.a.crawford@gmail.com>
2024-11-11 20:34:33 -0800

    Apply additional styles to signup component

--- app/javascript/components/signup/Signup.tsx
+++ app/javascript/components/signup/Signup.tsx
@@ -26,23 +26,14 @@ const Signup = () => {
   const { register, handleSubmit } = useForm<UserInput>();
 
   return (
-    <div>
+    <div className="p-4">
       {loading && <p>Loading</p>}
       <h1 className="text-2xl mb-2">Sign up for an account</h1>
       <p className="mb-4">
         An account will fill in your information to make ordering easier.
       </p>
 
-      <div className="w-1/2 grid place-content-center mx-auto">
-        {data?.userCreate?.errors?.map((error, i) => {
-          if (!error.path) return null;
-
-          return (
-            <p className="text-red-800" key={i}>
-              {startCase(error.path[1])} {error.message}
-            </p>
-          );
-        })}
+      <div className="md:w-[500px] md:mx-auto md:grid md:place-content-center">
         <form
           onSubmit={handleSubmit(async (formData) => {
             const mutationData = await createUser({
@@ -54,89 +45,24 @@ const Signup = () => {
             }
           })}
         >
-          <div className="md:flex md:gap-2">
-            <div className="mb-2">
-              <h3>Address</h3>
+          <div>
+            {data?.userCreate?.errors?.map((error, i) => {
+              if (!error.path) return null;
 
-              <div className="mb-2">
-                <label className="block" htmlFor="address.city">
-                  City
-                </label>
-                <input
-                  className="border-2 border-gray-600 p-2"
-                  id="address.city"
-                  {...register("address.city")}
-                />
-              </div>
-              <div className="mb-2">
-                <label className="block" htmlFor="address.country">
-                  Country
-                </label>
-                <input
-                  className="border-2 border-gray-600 p-2"
-                  id="address.country"
-                  defaultValue="USA"
-                  {...register("address.country")}
-                />
-              </div>
-              <div className="mb-2">
-                <label className="block" htmlFor="address.line1">
-                  Address 1
-                </label>
-                <input
-                  className="border-2 border-gray-600 p-2"
-                  id="address.line1"
-                  placeholder="ex: 123 Main St."
-                  {...register("address.line1")}
-                />
-              </div>
-              <div className="mb-2">
-                <label className="block" htmlFor="address.line2">
-                  Address 2
-                </label>
-                <input
-                  className="border-2 border-gray-600 p-2"
-                  id="address.line2"
-                  placeholder="ex: Unit 123, Apt 567"
-                  {...register("address.line2")}
-                />
-              </div>
-              <div className="mb-2">
-                <label className="block" htmlFor="address.postalCode">
-                  Postal Code
-                </label>
-                <input
-                  className="border-2 border-gray-600 p-2"
-                  id="address.postalCode"
-                  {...register("address.postalCode")}
-                />
-              </div>
-              <div className="mb-2">
-                <label className="block" htmlFor="address.state">
-                  State
-                </label>
-                <input
-                  className="border-2 border-gray-600 p-2"
-                  id="address.state"
-                  placeholder="ex: NV, CO, NY, AL"
-                  {...register("address.state")}
-                />
-              </div>
-              <div className="text-xs">
-                <p>
-                  This information will be used to create your Stripe Customer
-                  account, and is necessary for billing and delivery.{" "}
+              return (
+                <p className="text-red-800" key={i}>
+                  {startCase(error.path[1])} {error.message}
                 </p>
-                <p>
-                  <strong className="font-bold">IMPORTANT:</strong> This
-                  information will not be persisted in this app's database, but
-                  will instead be stored by Stripe.
-                </p>
-              </div>
-            </div>
+              );
+            })}
+          </div>
+          <div className="md:grid md:grid-cols-2 md:gap-10">
             <div className="mb-2">
-              <h3>Contact/Login Information</h3>
-              <div className="mb-2">
+              <h3 className="font-bold text-lg border-b-2 mb-4 ">
+                Address Information
+              </h3>
+
+              <div className="grid justify-center md:block">
                 <div className="mb-2">
                   <label className="block" htmlFor="name">
                     Full Name
@@ -148,6 +74,77 @@ const Signup = () => {
                     {...register("name")}
                   />
                 </div>
+                <div className="mb-2">
+                  <label className="block" htmlFor="address.city">
+                    City
+                  </label>
+                  <input
+                    className="border-2 border-gray-600 p-2"
+                    id="address.city"
+                    {...register("address.city")}
+                  />
+                </div>
+                <div className="mb-2">
+                  <label className="block" htmlFor="address.country">
+                    Country
+                  </label>
+                  <input
+                    className="border-2 border-gray-600 p-2"
+                    id="address.country"
+                    defaultValue="USA"
+                    {...register("address.country")}
+                  />
+                </div>
+                <div className="mb-2">
+                  <label className="block" htmlFor="address.line1">
+                    Address 1
+                  </label>
+                  <input
+                    className="border-2 border-gray-600 p-2"
+                    id="address.line1"
+                    placeholder="ex: 123 Main St."
+                    {...register("address.line1")}
+                  />
+                </div>
+                <div className="mb-2">
+                  <label className="block" htmlFor="address.line2">
+                    Address 2
+                  </label>
+                  <input
+                    className="border-2 border-gray-600 p-2"
+                    id="address.line2"
+                    placeholder="ex: Unit 123, Apt 567"
+                    {...register("address.line2")}
+                  />
+                </div>
+                <div className="mb-2">
+                  <label className="block" htmlFor="address.postalCode">
+                    Postal Code
+                  </label>
+                  <input
+                    className="border-2 border-gray-600 p-2"
+                    id="address.postalCode"
+                    {...register("address.postalCode")}
+                  />
+                </div>
+                <div className="mb-2">
+                  <label className="block" htmlFor="address.state">
+                    State
+                  </label>
+                  <input
+                    className="border-2 border-gray-600 p-2"
+                    id="address.state"
+                    placeholder="ex: NV, CO, NY, AL"
+                    {...register("address.state")}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="mb-2">
+              <h3 className="font-bold text-lg border-b-2 mb-4 ">
+                Contact/Login Information
+              </h3>
+              <div className="grid justify-center md:block">
                 <div className="mb-2">
                   <label className="block" htmlFor="phone">
                     Phone
@@ -197,7 +194,27 @@ const Signup = () => {
               </div>
             </div>
           </div>
-          <input className="text-center w-full green-button" type="submit" />
+          <div className="text-xs mb-4">
+            <p>
+              Address information, name and phone number will be used to create
+              your Stripe Customer account, and is necessary for billing and
+              delivery. This information will be required during the Stripe
+              Checkout, so it's recommended to fill it out now to save time
+              later.
+            </p>
+            <p>
+              <strong className="font-bold underline">Note:</strong> Only your
+              email and password will be stored by this app for logging in, the
+              rest of the information will be stored by Stripe and accessed by
+              this app via Stripe API for order delivery purposes (including
+              contacting you during the lifetime of an order, as necessary).
+            </p>
+          </div>
+          <input
+            className="text-center w-full green-button col-span-2"
+            type="submit"
+            value="Create Account"
+          />
         </form>
       </div>
     </div>
