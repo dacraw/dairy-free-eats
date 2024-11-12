f37753f6164458a89426b7bdd54a54dc62b7029c
Doug <doug.a.crawford@gmail.com>
2024-11-12 14:17:30 -0800

    Update order rspec

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Order, type: :model do
   context "associations" do
     it "sets a user if assigned" do
       user = create :user, :valid_user
-      order = build :order, user: user, stripe_id: "12345"
+      order = build :order, user: user, stripe_payment_intent_id: "12345"
 
       expect(order.user).to eq user
     end
@@ -12,23 +12,23 @@ RSpec.describe Order, type: :model do
 
   context "validations" do
     it "allows an Order to be created without a user" do
-      order = build :order, stripe_id: "pi_12345"
+      order = build :order, stripe_payment_intent_id: "pi_12345"
 
       expect { order.save }.to change { Order.count }.from(0).to(1)
     end
 
-    it "requires stripe_id to be present" do
+    it "requires stripe_payment_intent_id to be present" do
       order = build :order
 
       expect { order.save }.not_to change { Order.count }
 
-      expect(order.errors.full_messages).to include "Stripe can't be blank"
+      expect(order.errors.full_messages).to include "Stripe payment intent can't be blank"
     end
   end
 
   context "before the order is created" do
     it "sets the status to received" do
-      order = build :order, stripe_id: "pi_12345"
+      order = build :order, stripe_payment_intent_id: "pi_12345"
 
       expect { order.save }.to change { Order.count }.from(0).to(1)
 
