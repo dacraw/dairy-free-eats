c1473dcf5c22c3663e8298fc9644538ce34729bc
Doug <doug.a.crawford@gmail.com>
2024-11-14 02:58:24 -0800

    Test order successful component with a successful request

---
+++ app/javascript/components/orderSuccess/OrderSuccess.test.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import { screen, render } from "@testing-library/react";
+import OrderSuccess, {
+  FETCH_STRIPE_CHECKOUT_SESSION,
+} from "components/orderSuccess/OrderSuccess";
+import { MockedProvider, MockedResponse } from "@apollo/client/testing";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
+import {
+  FetchStripeCheckoutSessionQuery,
+  FetchStripeCheckoutSessionQueryVariables,
+} from "graphql/types";
+
+const validMocks: MockedResponse<
+  FetchStripeCheckoutSessionQuery,
+  FetchStripeCheckoutSessionQueryVariables
+>[] = [
+  {
+    request: {
+      query: FETCH_STRIPE_CHECKOUT_SESSION,
+      variables: {
+        id: "cs_test_a1D2NebdAudpPiucybElBFWapFaucAX27qC1lOyp8rRoLHef40fLy5vV0g",
+      },
+    },
+    result: {
+      data: {
+        fetchCheckoutSession: {
+          id: "cs_test_a1D2NebdAudpPiucybElBFWapFaucAX27qC1lOyp8rRoLHef40fLy5vV0g",
+          amountTotal: 624700,
+          lineItems: {
+            hasMore: false,
+            data: [
+              {
+                id: "li_1QKQTcElA4InVgv8UgpyYLXg",
+                amountTotal: 300,
+                description: "Mixed Berry Smoothie (Water base)",
+                quantity: 1,
+              },
+            ],
+          },
+        },
+      },
+    },
+  },
+];
+
+describe("<OrderSuccess />", () => {
+  describe("when there is a checkout_id search param", () => {
+    it("renders the order items and amount total", async () => {
+      render(
+        <MockedProvider mocks={validMocks}>
+          <MemoryRouter
+            future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
+            initialEntries={[
+              {
+                pathname: "/success",
+                search: "?checkout_id=" + validMocks[0].request.variables?.id,
+              },
+            ]}
+          >
+            <Routes>
+              <Route path="/success" element={<OrderSuccess />} />
+            </Routes>
+          </MemoryRouter>
+        </MockedProvider>
+      );
+
+      expect(await screen.findByText("Order Successful")).toBeInTheDocument();
+
+      expect(screen.getByText("$6,247.00")).toBeInTheDocument();
+      expect(
+        screen.getByText("Mixed Berry Smoothie (Water base) x1")
+      ).toBeInTheDocument();
+    });
+  });
+});
