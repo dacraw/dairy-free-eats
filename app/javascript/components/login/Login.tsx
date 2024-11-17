fd8a2f53418adc90a74620748f915ff182e5c9ca
Doug <doug.a.crawford@gmail.com>
2024-11-16 22:23:48 -0800

    Style login page to take up screen height

--- app/javascript/components/login/Login.tsx
+++ app/javascript/components/login/Login.tsx
@@ -12,46 +12,48 @@ const Login = () => {
   if (!data) return null;
 
   return (
-    <div className="grid place-content-center">
-      <form onSubmit={handleSubmit(login)} className="mb-12">
-        {error && <p className="text-red-700">{startCase(error.message)}</p>}
-        <h3 className="mb-4 text-2xl">Login</h3>
-        <div>
-          <label className="block" htmlFor="email">
-            Email
-          </label>
-          <input
-            id="email"
-            disabled={loading ? true : false}
-            className={`mb-2 text-center ${loading ? "blur" : ""}`}
-            {...register("email")}
-            type="email"
-          />
-        </div>
-        <div>
-          <label className="block" htmlFor="password">
-            Password
-          </label>
-          <input
-            id="password"
-            disabled={loading ? true : false}
-            minLength={8}
-            className={`mb-2 text-center ${loading ? "blur" : ""}`}
-            {...register("password")}
-            type="password"
-          />
-        </div>
-        <div>
-          <input
-            disabled={loading ? true : false}
-            type="submit"
-            className="w-full col-end-3 green-button"
-          />
-        </div>
-      </form>
+    <div className="grid align-top justify-center min-h-screen">
+      <div>
+        <form onSubmit={handleSubmit(login)} className="mb-12">
+          {error && <p className="text-red-700">{startCase(error.message)}</p>}
+          <h3 className="mb-4 text-2xl">Login</h3>
+          <div>
+            <label className="block" htmlFor="email">
+              Email
+            </label>
+            <input
+              id="email"
+              disabled={loading ? true : false}
+              className={`mb-2 text-center ${loading ? "blur" : ""}`}
+              {...register("email")}
+              type="email"
+            />
+          </div>
+          <div>
+            <label className="block" htmlFor="password">
+              Password
+            </label>
+            <input
+              id="password"
+              disabled={loading ? true : false}
+              minLength={8}
+              className={`mb-2 text-center ${loading ? "blur" : ""}`}
+              {...register("password")}
+              type="password"
+            />
+          </div>
+          <div>
+            <input
+              disabled={loading ? true : false}
+              type="submit"
+              className="w-full col-end-3 green-button"
+            />
+          </div>
+        </form>
 
-      <div className="text-center">
-        <Link to="/password_reset">Forgot Password?</Link>
+        <div className="text-center">
+          <Link to="/password_reset">Forgot Password?</Link>
+        </div>
       </div>
     </div>
   );
