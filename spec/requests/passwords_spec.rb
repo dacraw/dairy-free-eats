3018689d9678bea5fd6bf89d597ae16018badf0b
Doug <doug.a.crawford@gmail.com>
2024-11-16 17:36:43 -0800

    Write spec for updating password through password reset

--- spec/requests/passwords_spec.rb
+++ spec/requests/passwords_spec.rb
@@ -18,4 +18,42 @@ RSpec.describe "Passwords", type: :request do
       end
     end
   end
+
+  describe "PATCH /password" do
+    context "when the user has a valid password reset token" do
+      let(:user) { create :user, :valid_user }
+
+      context "when the password and password confirmation matches" do
+        it "updates the user's password" do
+          new_password = Faker::Internet.password min_length: 8
+
+          expect {
+            patch password_path(token: user.password_reset_token), params: { 
+              password: new_password,
+              password_confirmation: new_password
+            }
+          }.to change { user.reload.password_digest }
+
+          response_body = JSON.parse(response.body)
+          expect(response_body["message"]).to eq "success"
+          expect(response_body["redirect_url"]).to eq "/login"
+        end
+      end
+
+      context "when there is an unmatching password and password confirmation" do
+        it "returns a json error" do
+          expect {
+            patch password_path(token: user.password_reset_token), params: { 
+              password: "new_password",
+              password_confirmation: "does_not_match"
+            }
+          }.not_to change { user.reload.password_digest }
+
+          response_body = JSON.parse(response.body)
+          expect(response_body["message"]).to eq "failed"
+          expect(response_body["error"]).to eq "Passwords did not match."
+        end
+      end
+    end
+  end
 end
