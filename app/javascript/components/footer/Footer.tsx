03e3cc582c96dee372bc4fcee82a11c4143bcfd6
Doug <doug.a.crawford@gmail.com>
2024-11-16 22:12:31 -0800

    Create footer

---
+++ app/javascript/components/footer/Footer.tsx
@@ -0,0 +1,51 @@
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="grid place-content-center border-t-2 my-4 py-4 text-center bg-blue-900">
+      <div className="mb-4">
+        <p className="font-bold text-lg">Doug Crawford</p>
+        <p>
+          <a href="mailto: doug.a.crawford@gmail.com">
+            doug.a.crawford@gmail.com
+          </a>
+        </p>
+        <p>760-219-0584</p>
+      </div>
+      <div>
+        <a
+          href="https://dacraw.github.io"
+          className="font-bold mb-4 block hover:underline"
+          target="_blank"
+        >
+          &rArr; View My Portfolio &lArr;
+        </a>
+        <div className="flex gap-4 justify-center">
+          <a
+            target="_blank"
+            href="https://www.linkedin.com/in/doug-a-crawford/"
+          >
+            <FontAwesomeIcon
+              size="lg"
+              className="hover:opacity-80 transition-opacity duration-300"
+              bounce
+              icon={faLinkedin}
+            />
+          </a>
+          <a target="_blank" href="https://github.com/dacraw">
+            <FontAwesomeIcon
+              size="lg"
+              className="hover:opacity-80 transition-opacity duration-300"
+              bounce
+              icon={faGithub}
+            />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
