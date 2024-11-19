2f824dadb7ebfe7b1033d61caa73c392cf194589
Doug <doug.a.crawford@gmail.com>
2024-11-18 23:36:00 -0800

    Store order in instance variable

--- app/channels/orders_channel.rb
+++ app/channels/orders_channel.rb
@@ -1,13 +1,14 @@
 class OrdersChannel < ApplicationCable::Channel
   def subscribed
-    order = Order.find_by(id: params[:id])
+    @order ||= Order.find_by(id: params[:id])
+    @sent_message_ids = []
 
-    if order.nil?
-      puts "Order ##{order.id} does not exist"
+    if @order.nil?
+      puts "Order ##{@order.id} does not exist"
       return
     end
 
-    stream_for order
+    stream_for @order
   end
 
   def unsubscribed
