bd97067a346bc41bdc75f9f5a46cd0ed679bf5ad
Doug <doug.a.crawford@gmail.com>
2024-11-18 23:36:35 -0800

    Broadcast after create commit instead of delete

--- app/models/order_message.rb
+++ app/models/order_message.rb
@@ -2,5 +2,5 @@ class OrderMessage < ApplicationRecord
   belongs_to :order
   belongs_to :user
 
-  after_commit { NewOrderMessageJob.perform_later self }
+  after_create_commit { NewOrderMessageJob.perform_later self }
 end
