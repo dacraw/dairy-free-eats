7800e9d52475cc2af63cc984d516689d51d8d3c1
Doug <doug.a.crawford@gmail.com>
2024-11-18 23:36:20 -0800

    Broadcast only message body

--- app/jobs/new_order_message_job.rb
+++ app/jobs/new_order_message_job.rb
@@ -3,6 +3,6 @@ class NewOrderMessageJob < ApplicationJob
 
   def perform(order_message)
     # Do something later
-    OrdersChannel.broadcast_to order_message.order, order_message
+    OrdersChannel.broadcast_to order_message.order, body: order_message.body
   end
 end
