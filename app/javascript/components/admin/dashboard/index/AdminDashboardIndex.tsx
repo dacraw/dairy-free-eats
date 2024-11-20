b9a559e9a0c6f6f07eec663b0b0667973a58e7e2
Doug <doug.a.crawford@gmail.com>
2024-11-20 14:20:19 -0800

    Add descriptions of admin dashboard pages

--- app/javascript/components/admin/dashboard/index/AdminDashboardIndex.tsx
+++ app/javascript/components/admin/dashboard/index/AdminDashboardIndex.tsx
@@ -5,9 +5,17 @@ const AdminDashboardIndex = () => {
     <div>
       <p className="mb-6">Select a navigation option from above.</p>
 
-      <p className="text-sm">
-        <strong className="font-bold">Note:</strong> Order chat is on the way!
-      </p>
+      <div>
+        <p className="mb-4">
+          <strong className="font-bold">ORDERS:</strong>View all orders placed
+          by a user, whether a guest or a user registered on this site.
+        </p>
+        <p className="mb-4">
+          <strong className="font-bold">ORDER CHATS:</strong>View order chat
+          panels for any chat that is not completed. Only the user that placed
+          the order and any admin can send messages in this chat.
+        </p>
+      </div>
     </div>
   );
 };
