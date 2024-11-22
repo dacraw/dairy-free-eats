cd319b385815e82b5db4d6c626114297d415a630
Doug <doug.a.crawford@gmail.com>
2024-11-21 17:21:04 -0800

    Add borders beneath each notification

--- app/javascript/components/headerNav/headerNotifications/HeaderNotifications.tsx
+++ app/javascript/components/headerNav/headerNotifications/HeaderNotifications.tsx
@@ -27,12 +27,16 @@ const NotificationsList = () => {
   const { data, loading } = useFetchCurrentUserNotificationsQuery();
 
   return (
-    <div className="relative">
+    <div className="relative text-left">
       <div className="absolute rounded h-96 overflow-auto bg-gray-700 p-4 md:w-[300px] right-0 top-2">
-        <h3 className="font-bold text-center">NOTIFICATIONS</h3>
+        <h3 className="font-bold text-center border-b-2 mb-2 pb-2">
+          NOTIFICATIONS
+        </h3>
         <div>
           {data?.currentUserNotifications?.map((notification) => (
-            <p key={notification.id}>{notification.message}</p>
+            <p key={notification.id} className="border-b-2 mb-2 pb-2">
+              {notification.message}
+            </p>
           ))}
         </div>
       </div>
