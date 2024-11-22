b915600a634fe3d1a3b258eac6acd0835b191884
Doug <doug.a.crawford@gmail.com>
2024-11-21 16:45:43 -0800

    Display notification when order message sent to user

--- app/javascript/components/headerNav/headerNotifications/HeaderNotifications.tsx
+++ app/javascript/components/headerNav/headerNotifications/HeaderNotifications.tsx
@@ -1,16 +1,65 @@
-import { faBell } from "@fortawesome/free-solid-svg-icons";
+import {
+  faBell,
+  faCircle,
+  faDotCircle,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useEffect, useState } from "react";
 import consumer from "channels/consumer";
-import { CurrentUserQuery } from "graphql/types";
+import {
+  CurrentUserQuery,
+  useCurrentUserNotificationReceivedSubscription,
+  useFetchCurrentUserNotificationsQuery,
+} from "graphql/types";
+import { gql, useSubscription } from "@apollo/client";
+
+export const FETCH_CURRENT_USER_NOTIFICATIONS = gql`
+  query FetchCurrentUserNotifications {
+    currentUserNotifications {
+      id
+      message
+      path
+    }
+  }
+`;
 
 const NotificationsList = () => {
+  const { data, loading } = useFetchCurrentUserNotificationsQuery();
+
   return (
     <div className="relative">
-      <div className="absolute rounded bg-gray-700 p-4 md:w-[300px] right-0 top-2">
+      <div className="absolute rounded h-96 overflow-auto bg-gray-700 p-4 md:w-[300px] right-0 top-2">
         <h3 className="font-bold text-center">NOTIFICATIONS</h3>
-        <p>hey</p>
+        <div>
+          {data?.currentUserNotifications?.map((notification) => (
+            <p>{notification.message}</p>
+          ))}
+        </div>
       </div>
+    </div>
+  );
+};
+
+const CURRENT_USER_NOTIFICATION_RECEIVED = gql`
+  subscription CurrentUserNotificationReceived {
+    currentUserNotificationReceived {
+      notification {
+        id
+        message
+        userId
+      }
+    }
+  }
+`;
+
+const NotificationPopup = ({
+  notificationMessage,
+}: {
+  notificationMessage: string;
+}) => {
+  return (
+    <div className="fixed right-0 top-1/2 transform -translate-y-1/2 p-2 rounded bg-gray-700">
+      <p>{notificationMessage}</p>
     </div>
   );
 };
@@ -21,32 +70,53 @@ const HeaderNotifications = ({
   currentUser: CurrentUserQuery["currentUser"];
 }) => {
   const [openList, toggleOpenList] = useState(false);
+  const [showNotificationPopup, setShowNotificationPopup] = useState(false);
+  const [showRedDot, setShowRedDot] = useState(false);
+  const { data, loading } = useCurrentUserNotificationReceivedSubscription();
 
   useEffect(() => {
-    consumer.subscriptions.create("NotificationsChannel", {
-      connected() {
-        console.log("connected in header notifications");
-        // Called when the subscription is ready for use on the server
-      },
+    if (data?.currentUserNotificationReceived && !openList) {
+      setShowNotificationPopup(true);
 
-      disconnected() {
-        // Called when the subscription has been terminated by the server
-      },
+      if (!openList) {
+        setShowRedDot(true);
+      }
 
-      received(data) {
-        // Called when there's incoming data on the websocket for this channel
-        console.log(data);
-      },
-    });
-  }, []);
+      const timeoutId = setTimeout(() => {
+        setShowNotificationPopup(false);
+      }, 3000);
+
+      return () => clearTimeout(timeoutId);
+    }
+  }, [data]);
 
   return (
-    <div>
+    <div className="relative">
       <FontAwesomeIcon
-        onClick={() => toggleOpenList(!openList)}
-        className="cursor-pointer hover:text-blue-200"
+        onClick={() => {
+          if (!openList) {
+            setShowRedDot(false);
+          }
+          toggleOpenList(!openList);
+        }}
+        className={`cursor-pointer hover:text-blue-200 text-xl`}
         icon={faBell}
       />
+
+      {showRedDot && (
+        <FontAwesomeIcon
+          icon={faCircle}
+          className="text-red-700 text-sm absolute left-[10px] -top-[5px]"
+        />
+      )}
+
+      {showNotificationPopup && (
+        <NotificationPopup
+          notificationMessage={
+            data?.currentUserNotificationReceived?.notification?.message || ""
+          }
+        />
+      )}
 
       {openList && <NotificationsList />}
     </div>
