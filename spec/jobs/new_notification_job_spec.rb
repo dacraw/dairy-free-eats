9e7b96b35bde2222cd50c666d6ef5659037832cb
Doug <doug.a.crawford@gmail.com>
2024-11-23 13:53:42 -0800

    Fix existing test issues

--- spec/jobs/new_notification_job_spec.rb
+++ spec/jobs/new_notification_job_spec.rb
@@ -3,14 +3,15 @@ require 'rails_helper'
 RSpec.describe NewNotificationJob, type: :job do
   it "broadcasts" do
     notification = create :notification, user: create(:user, :valid_user), message: "heyo"
+    subs_double = double GraphQL::Subscriptions::ActionCableSubscriptions
 
-    expect(NotificationsChannel)
-      .to receive(:broadcast_to)
-      .with(
-        notification.user,
-        message: notification.message,
-        path: notification.path
-      )
+    expect(DairyFreeFoodSchema).to receive(:subscriptions) { subs_double }
+    expect(subs_double).to receive(:trigger).with(
+      :current_user_notification_received,
+      {},
+      { notification: notification },
+      scope: notification.user
+    )
 
     NewNotificationJob.perform_now notification
   end
