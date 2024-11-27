3c25a5957155435e13b68f252e69c3d58aea4e5e
Doug <doug.a.crawford@gmail.com>
2024-11-27 00:07:23 -0800

    Create responsive header index link

--- app/javascript/components/headerNav/HeaderNav.tsx
+++ app/javascript/components/headerNav/HeaderNav.tsx
@@ -1,7 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useEffect, useRef, useState } from "react";
 import { faBars, faSpinner } from "@fortawesome/free-solid-svg-icons";
-import { NavLink, useLocation, useNavigate } from "react-router-dom";
+import { Link, NavLink, useLocation, useNavigate } from "react-router-dom";
 import { gql } from "@apollo/client";
 import { useCurrentUserLazyQuery } from "graphql/types";
 import { useAdminLogin, useLogout } from "hooks/auth";
@@ -211,10 +211,13 @@ const HeaderNav = () => {
 
   return (
     <header className="shadow-md bg-gradient-to-b from-gray-900 to-gray-950 shadow-gray-950 fixed w-full p-2 select-none h-[50px] z-50">
-      <nav className="relative flex justify-end gap-4 items-center md:static md:grid md:grid-cols-[1fr_auto_auto] md:grid-rows-1 md:justify-between max-w-screen-lg mx-auto">
+      <nav className="relative gap-4 items-center my-2 mx-4 md:static grid grid-cols-[1fr_auto_auto] md:grid-rows-1 md:justify-between max-w-screen-lg md:mx-auto">
         {error && <span>{error.message}</span>}
+        <Link to="/" className="md:hidden justify-self-start font-bold">
+          Dairy Free Eats
+        </Link>
         {data?.currentUser && (
-          <div className="md:col-start-2 md:row-start-1 md:justify-self-end">
+          <div className="md:col-start-2 md:row-start-1 justify-self-end">
             <HeaderNotifications />
           </div>
         )}
