2fcf31a5945ea04d43cdd58b960a67cf5a96d9bb
Doug <doug.a.crawford@gmail.com>
2024-12-02 15:33:51 -0800

    Reinstate stripe product api ping

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -35,17 +35,7 @@ module Types
 
     field :list_products, Types::Stripe::ProductListObjectType, null: false
     def list_products
-      # ::Stripe::Product.list active: true, expand: ['data.default_price']
-
-      ::Stripe::ListObject.construct_from({
-      "object": "list",
-      "data": [
-        {"id":"prod_R8KkVOibvpRPlc","object":"product","active":true,"attributes":[],"created":1730403020,"default_price":{"id":"price_1QG44XElA4InVgv8dOnqfhyu","object":"price","active":true,"billing_scheme":"per_unit","created":1730403021,"currency":"usd","custom_unit_amount":nil,"livemode":false,"lookup_key":nil,"metadata":{},"nickname":nil,"product":"prod_R8KkVOibvpRPlc","recurring":nil,"tax_behavior":"unspecified","tiers_mode":nil,"transform_quantity":nil,"type":"one_time","unit_amount":300,"unit_amount_decimal":"300"},"description":"Blended mixed berries, filtered water","images":["https://files.stripe.com/links/MDB8YWNjdF8xUUczb3lFbEE0SW5WZ3Y4fGZsX3Rlc3RfUGczWDd3YkUzR1JBY2tOck14NEVnYXpp00CBDSqGR6"],"livemode":false,"marketing_features":[],"metadata":{},"name":"Mixed Berry Smoothie (Water base)","package_dimensions":nil,"shippable":nil,"statement_descriptor":nil,"tax_code":"txcd_20030000","type":"service","unit_label":nil,"updated":1732761774,"url":nil},
-        {"id":"prod_R8KiBa0tljS7SV","object":"product","active":true,"attributes":[],"created":1730402927,"default_price":{"id":"price_1QG432ElA4InVgv8Zy7PaGPY","object":"price","active":true,"billing_scheme":"per_unit","created":1730402928,"currency":"usd","custom_unit_amount":nil,"livemode":false,"lookup_key":nil,"metadata":{},"nickname":nil,"product":"prod_R8KiBa0tljS7SV","recurring":nil,"tax_behavior":"unspecified","tiers_mode":nil,"transform_quantity":nil,"type":"one_time","unit_amount":300,"unit_amount_decimal":"300"},"description":"2 salted/peppered eggs, 2 strips of bacon, hummis","images":["https://files.stripe.com/links/MDB8YWNjdF8xUUczb3lFbEE0SW5WZ3Y4fGZsX3Rlc3RfbUNoWnRHazJyN3JGVDNMZTZjM1dua0xh008k0zo7yY"],"livemode":false,"marketing_features":[],"metadata":{},"name":"Breakfast Burrito","package_dimensions":nil,"shippable":nil,"statement_descriptor":nil,"tax_code":"txcd_20030000","type":"service","unit_label":nil,"updated":1732756272,"url":nil}
-      ],
-      "has_more": false,
-      "url": "/v1/products"
-    })
+      ::Stripe::Product.list active: true, expand: [ "data.default_price" ]
     end
 
     field :fetch_checkout_session, resolver: Resolvers::Stripe::CheckoutSessionResolver, null: true
