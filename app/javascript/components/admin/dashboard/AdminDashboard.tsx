5096448d9e5638a03c3c99a91bdd8135bd3a68d0
Doug <doug.a.crawford@gmail.com>
2024-12-05 01:15:24 -0800

    Update conditional

--- app/javascript/components/admin/dashboard/AdminDashboard.tsx
+++ app/javascript/components/admin/dashboard/AdminDashboard.tsx
@@ -5,55 +5,57 @@ import React, { useEffect } from "react";
 import { NavLink, Outlet, useNavigate } from "react-router-dom";
 
 const AdminDashboard = () => {
-  const { loading: currentUserLoading, data: currentUserData } =
-    useCurrentUserQuery();
+  const {
+    loading: currentUserLoading,
+    data: currentUserData,
+    error,
+  } = useCurrentUserQuery({ fetchPolicy: "network-only" });
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (!currentUserData?.currentUser?.admin) {
+    if (
+      (currentUserData?.currentUser && !currentUserData?.currentUser?.admin) ||
+      currentUserData?.currentUser === null
+    ) {
       navigate("/");
     }
   }, [currentUserData]);
 
   return (
     <div className="md:w-[90vw]">
-      {currentUserLoading ? (
-        <FontAwesomeIcon icon={faSpinner} />
-      ) : (
-        <>
-          <h3 className="font-bold md:px-6 text-center text-xl mb-6 animate-home-title-shimmer">
-            Admin Dashboard
-          </h3>
-          <div className="flex gap-4 border-b-2 pb-2 mb-2">
-            <NavLink
-              className={({ isActive }) => {
-                return `${
-                  isActive
-                    ? "blue-button"
-                    : "hover:text-blue-400 transition-colors"
-                } py-2 px-4  text-lg font-bold`;
-              }}
-              to="orders"
-            >
-              ORDERS
-            </NavLink>
-            <NavLink
-              className={({ isActive }) => {
-                return `${
-                  isActive
-                    ? "blue-button"
-                    : "hover:text-blue-400 transition-colors"
-                } py-2 px-4  text-lg font-bold`;
-              }}
-              to="order_chats"
-            >
-              ORDER CHATS
-            </NavLink>
-          </div>
+      <>
+        <h3 className="font-bold md:px-6 text-center text-xl mb-6 animate-home-title-shimmer">
+          Admin Dashboard
+        </h3>
+        <div className="flex gap-4 border-b-2 pb-2 mb-2">
+          <NavLink
+            className={({ isActive }) => {
+              return `${
+                isActive
+                  ? "blue-button"
+                  : "hover:text-blue-400 transition-colors"
+              } py-2 px-4  text-lg font-bold`;
+            }}
+            to="orders"
+          >
+            ORDERS
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => {
+              return `${
+                isActive
+                  ? "blue-button"
+                  : "hover:text-blue-400 transition-colors"
+              } py-2 px-4  text-lg font-bold`;
+            }}
+            to="order_chats"
+          >
+            ORDER CHATS
+          </NavLink>
+        </div>
 
-          <Outlet />
-        </>
-      )}
+        <Outlet />
+      </>
     </div>
   );
 };
