92810e22d0a85238ecf8800c3c26eed65ba85cfb
Doug <doug.a.crawford@gmail.com>
2024-12-05 00:20:51 -0800

    Remove check for current user before determining redirect

--- app/javascript/components/admin/dashboard/AdminDashboard.tsx
+++ app/javascript/components/admin/dashboard/AdminDashboard.tsx
@@ -1,3 +1,5 @@
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useCurrentUserQuery } from "graphql/types";
 import React, { useEffect } from "react";
 import { NavLink, Outlet, useNavigate } from "react-router-dom";
@@ -8,40 +10,50 @@ const AdminDashboard = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (currentUserData?.currentUser && !currentUserData.currentUser.admin) {
+    if (!currentUserData?.currentUser?.admin) {
       navigate("/");
     }
   }, [currentUserData]);
 
   return (
     <div className="md:w-[90vw]">
-      <h3 className="font-bold md:px-6 text-center text-xl mb-6 animate-home-title-shimmer">
-        Admin Dashboard
-      </h3>
-      <div className="flex gap-4 border-b-2 pb-2 mb-2">
-        <NavLink
-          className={({ isActive }) => {
-            return `${
-              isActive ? "blue-button" : "hover:text-blue-400 transition-colors"
-            } py-2 px-4  text-lg font-bold`;
-          }}
-          to="orders"
-        >
-          ORDERS
-        </NavLink>
-        <NavLink
-          className={({ isActive }) => {
-            return `${
-              isActive ? "blue-button" : "hover:text-blue-400 transition-colors"
-            } py-2 px-4  text-lg font-bold`;
-          }}
-          to="order_chats"
-        >
-          ORDER CHATS
-        </NavLink>
-      </div>
+      {currentUserLoading ? (
+        <FontAwesomeIcon icon={faSpinner} />
+      ) : (
+        <>
+          <h3 className="font-bold md:px-6 text-center text-xl mb-6 animate-home-title-shimmer">
+            Admin Dashboard
+          </h3>
+          <div className="flex gap-4 border-b-2 pb-2 mb-2">
+            <NavLink
+              className={({ isActive }) => {
+                return `${
+                  isActive
+                    ? "blue-button"
+                    : "hover:text-blue-400 transition-colors"
+                } py-2 px-4  text-lg font-bold`;
+              }}
+              to="orders"
+            >
+              ORDERS
+            </NavLink>
+            <NavLink
+              className={({ isActive }) => {
+                return `${
+                  isActive
+                    ? "blue-button"
+                    : "hover:text-blue-400 transition-colors"
+                } py-2 px-4  text-lg font-bold`;
+              }}
+              to="order_chats"
+            >
+              ORDER CHATS
+            </NavLink>
+          </div>
 
-      <Outlet />
+          <Outlet />
+        </>
+      )}
     </div>
   );
 };
