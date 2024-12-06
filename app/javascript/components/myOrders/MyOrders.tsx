8b3feef568d0741d017960e5f03edab40e9e92ce
Doug <doug.a.crawford@gmail.com>
2024-12-05 23:12:03 -0800

    Create view template for my orders

--- app/javascript/components/myOrders/MyOrders.tsx
+++ app/javascript/components/myOrders/MyOrders.tsx
@@ -1,9 +1,34 @@
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useFetchCurrentUserOrdersQuery } from "graphql/types";
 import React from "react";
 
 const MyOrders = () => {
+  const { data, loading, error } = useFetchCurrentUserOrdersQuery();
+
+  if (!data) return null;
+
   return (
     <div>
-      <p>hey</p>
+      {loading ? (
+        <FontAwesomeIcon icon={faSpinner} />
+      ) : (
+        <div>
+          {data?.currentUserOrders?.map((order) => (
+            <div key={order.id}>
+              <p>{order.status}</p>
+              <div>
+                {order.stripeCheckoutSessionLineItems.map((item, i) => (
+                  <div key={i}>
+                    <p>{item.name}</p>
+                    <p>{item.quantity}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
